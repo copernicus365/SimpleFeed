dbddb00f56c31bb603207748c2f6ee7c1399fb58
Nicholas Petersen <copernicus365@gmail.com>
2019-02-20 04:57:03 -0500

    Added basic test project: SimpleFeed.Tests

---
+++ SimpleFeed.Tests/src/FeedTestsBase.cs
@@ -0,0 +1,52 @@
+using System;
+using Xunit;
+using DotNetXtensions;
+using DotNetXtensions.Net;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SimpleFeedNS.Tests
+{
+	public class FeedTestsBase
+	{
+		protected string _testFeedUrl;
+		protected string _testFeedPath;
+		protected string _feedContent;
+		protected SimpleFeed _feed;
+
+		public async Task<SimpleFeed> SetFeed(
+			bool saveToFileIfPossible = true,
+			Action<SFFeedSettings> alterSettings = null)
+		{
+			if (_testFeedPath.NotNulle()) {
+				_feedContent = await File.ReadAllTextAsync(_testFeedPath);
+			}
+			else if (_testFeedUrl.NotNulle()) {
+				_feedContent = await XHttp.GetStringAsync(_testFeedUrl);
+
+				if (saveToFileIfPossible && _feedContent.NotNulle() && _testFeedPath.NotNulle()) {
+					await File.WriteAllTextAsync(_testFeedPath, _feedContent);
+				}
+			}
+
+			Assert.True(_feedContent.NotNulle());
+
+			var settings = new SFFeedSettings() {
+				KeepXmlDocument = true,
+				ConvertCategoryUrlsToLinks = true,
+				ConvertContentUrlsToLinks = true,
+				AlterUTCDatesToThisTimezone = TimeZoneInfo.Local,
+				GetImageUrlsFromContentImgTag = true,
+			};
+
+			alterSettings?.Invoke(settings);
+
+			_feed = new SimpleFeed {
+				Settings = settings
+			};
+
+			return _feed;
+		}
+
+	}
+}
